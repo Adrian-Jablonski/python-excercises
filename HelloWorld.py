b855911b5a6168eb3d40434b55a6e27e5a3ac9c1
Adrian Jablonski <adrianj@Adrians-Air.attlocal.net>
2018-02-05 19:14:25 -0600

    Class 1 notes

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+print("Hello", "World")
+
+import sys
+
+sys.stdout.write("Hello")
+
+sys.stdout.write("World")
+
+"hello world".capitalize()
+
+"hello world".upper()
---
+++ logic.py
@@ -0,0 +1,8 @@
+age = int(input("How old are you: "))
+
+if age >= 21:
+    print("You get free beer")
+elseif age < 18:
+    print("What are you even doing here")
+else :
+    print("No beer for you")
